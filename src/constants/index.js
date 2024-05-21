3d2eef17da23c7e05e01b6bf1ef9f241824a1714
Dongseok <117716335+Dongjewerly@users.noreply.github.com>
2024-05-21 14:06:35 +0900

    헤더영역완료

---
+++ src/constants/index.js
@@ -0,0 +1,22 @@
+export const headerNav = [
+    {
+      title: "intro",
+      url:"#intro",
+    }, 
+    {
+      title: "skill",
+      url:"#skill",
+    },
+    {
+      title: "site",
+      url:"#site",
+    },
+    {
+      title: "port",
+      url:"#port",
+    },
+    {
+      title: "contact",
+      url:"#contact",
+    },
+  ];
